af196ae33423f93b50ebd8c0920b0fe0cf0bb8f2
sshyuny <thgustls@gmail.com>
2024-09-04 21:59:52 +0900

    feat: validation - rejectValue 메서드 사용

--- src/main/java/com/sshyuny/validation/ValidationController.java
+++ src/main/java/com/sshyuny/validation/ValidationController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sshyuny.validation.dto.BookReqDto;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class ValidationController {
 
@@ -24,7 +27,7 @@ public class ValidationController {
      * 
      * [일반] curl -X POST -d "isbn=1122334455&price=12000" http://localhost:8080/validation/myself/by-using-addError
      * [타입에러 컨트롤러 진입 가능!] curl -X POST -d "isbn=1122334455&price=twothousand" http://localhost:8080/validation/myself/by-using-addError
-     * [isbn, pricd 에러] curl -X POST -d "isbn=11223&pri" http://localhost:8080/validation/myself/by-using-addError
+     * [isbn, price 에러] curl -X POST -d "isbn=11223&pri" http://localhost:8080/validation/myself/by-using-addError
      * [글로벌에러] curl -X POST -d "isbn=1122334455&price=12000&title=abcdefghijklmnopqrstuvwx&subTitle=abcdefghijklmnopqrstuvwx" http://localhost:8080/validation/myself/by-using-addError
      */
     @PostMapping("/validation/myself/by-using-addError")
@@ -96,6 +99,55 @@ public class ValidationController {
         System.out.println("검증 통과!");
         return "validation-success";
     }
+
+    /*
+     * BindingResult.rejectValue 메서드 사용
+     * 
+     * [일반] curl -X POST -d "isbn=1122334455&price=12000" http://localhost:8080/validation/myself/by-using-rejectValue
+     * [타입에러 컨트롤러 진입 가능!] curl -X POST -d "isbn=1122334455&price=twothousand" http://localhost:8080/validation/myself/by-using-rejectValue
+     * [isbn, price 에러] curl -X POST -d "isbn=11223&pri" http://localhost:8080/validation/myself/by-using-rejectValue
+     * [글로벌에러] curl -X POST -d "isbn=1122334455&price=12000&title=abcdefghijklmnopqrstuvwx&subTitle=abcdefghijklmnopqrstuvwx" http://localhost:8080/validation/myself/by-using-rejectValue
+     */
+    @PostMapping("/validation/myself/by-using-rejectValue")
+    public String myselfUsingRejectValue(@ModelAttribute BookReqDto bookReqDto, BindingResult bindingResult) {
+        System.out.println(bookReqDto);
+
+        log.info("objectName={}" + bindingResult.getObjectName());
+        log.info("target={}" + bindingResult.getTarget());
+
+        String isbn = bookReqDto.getIsbn();
+        Integer price = bookReqDto.getPrice();
+        String title = bookReqDto.getTitle();
+        String subTitle = bookReqDto.getSubTitle();
+
+        if (isbn == null || isbn.isBlank() || isbn.length() < 10 || isbn.length() > 13) {
+            // bindingResult.addError(new FieldError("bookReqDto", "isbn", isbn, false, new String[] {"length.bookReqDto.isbn"}, null, null));
+            bindingResult.rejectValue("isbn", "length");
+        }
+
+        if (price == null || price <= 1000 || price >= 10000000) {
+            // bindingResult.addError(new FieldError("bookReqDto", "price", price, false, new String[] {"range.bookReqDto.price"}, new Object[] {1000, 1000000}, null));
+            bindingResult.rejectValue("price", "range", new Object[] {1000, 1000000}, null);
+        }
+
+        if (title != null && subTitle != null) {
+            int totalTitleLength = title.length() + subTitle.length();
+            if (totalTitleLength > 24) {
+                // bindingResult.addError(new ObjectError("bookReqDto", new String[] {"totalTitleMax"}, new Object[] {24, totalTitleLength}, null));
+                bindingResult.reject("totalTitleMax", new Object[] {24, totalTitleLength}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("검증 실패!");
+            return "validation-hasError";
+        }
+        
+        System.out.println("검증 통과!");
+        return "validation-success";
+    }
+
+
 
 
 }
